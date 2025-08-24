61c64aa0a6cc459a500e148bee0652db6e3c6630
juun0-h <juun0.han@gmail.com>
2025-08-25 03:27:20 +0900

    feat: FastAPI 애플리케이션 라이프사이클 관리 추가
    - lifespan 컨텍스트 매니저로 앱 시작/종료 이벤트 관리
    - 데이터베이스 엔진 graceful shutdown 구현
    - 애플리케이션 시작/종료 로그 추가
    - 리소스 누수 방지를 위한 정리 로직 구현

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from routes import blog
+from contextlib import asynccontextmanager
+from db.database import engine
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
+    print("#######Starting up...")
+    yield
+    
+    # Shutdown
+    print("#######Shutting down...")
+    await engine.dispose()
+    
+app = FastAPI(lifespan=lifespan)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(blog.router)
